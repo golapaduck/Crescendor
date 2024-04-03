94703679cf03a6a416c5db85364a68a8a09f203d
golapaduck <golapaduck29@naver.com>
2024-04-04 04:10:52 +0900

    feat: getlog date만 리턴

--- Server/index.js
+++ Server/index.js
@@ -103,7 +103,7 @@ app.get('/log', (req, res) => {
 app.get('/log/getlog/:user_id', (req, res) => {
   const user_id = req.params.user_id
 
-  pool.query(`SELECT * FROM Crescendor.log where id = "${user_id}";`, (error, rows) => {
+  pool.query(`SELECT date FROM Crescendor.log where id = "${user_id}";`, (error, rows) => {
     if (error){
       res.status(400).send('ERROR: Data')
       return
